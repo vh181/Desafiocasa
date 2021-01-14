342f050d8c9b82a42af41ac50421de2e930e2652
victor <dinizvictorhugo18@gmail.com>
2021-01-14 17:55:06 -0300

    4

--- app/src/main/java/com/gv/desafio/MainActivity.kt
+++ app/src/main/java/com/gv/desafio/MainActivity.kt
@@ -2,6 +2,10 @@ package com.gv.desafio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gv.desafio.data.fakeGit
+import com.gv.desafio.model.GitAdapter
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+
+        recycler_view_main.adapter=GitAdapter(fakeGit())
+        recycler_view_main.layoutManager = LinearLayoutManager(this)
     }
 }
